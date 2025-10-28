1cac47570d0fe273b46c60fa0be205298a71a9ac
CesarV03 <3021762@wcupa.edu>
2025-10-28 17:25:24 -0400

    Node.java

--- Node.java
+++ Node.java
@@ -1,99 +1,73 @@
-import java.util.Random;
-
-public class Node implements Cloneable {
-    protected Node left;
-    protected Node right;
-    protected Op operation;
-    protected int depth;
+// imported first from grasp
+public class Node {
+    private Binop op;
+    private Node left;
+    private Node right;
+    private Const constant;
+    private Variable variable;
 
     public Node(Binop op, Node left, Node right) 
     {
-        this.operation = op;
+        this.op = op;
         this.left = left;
         this.right = right;
-        this.depth = 0;
     }
 
-    public Node(Unop op) 
+    public Node(Const constant) 
     {
-        this.operation = op;
-        this.left = null;
-        this.right = null;
-        this.depth = 0;
+        this.constant = constant;
     }
 
-    public Node(Binop op) 
+    public Node(Variable variable) 
     {
-        this.operation = op;
-        this.left = null;
-        this.right = null;
-        this.depth = 0;
+        this.variable = variable;
     }
-    
-    public void addRandomKids(NodeFactory nf, int maxDepth, Random rand) {
-        if (operation instanceof Unop) {
-            return;
-        }
 
-        if (depth >= maxDepth) {
-            left = nf.getTerminal(rand);
-            left.depth = depth + 1;
-            right = nf.getTerminal(rand);
-            right.depth = depth + 1;
-            return;
-        }
-
-        int choiceLeft = rand.nextInt(nf.getNumOps() + nf.getNumIndepVars() + 1);
-        if (choiceLeft < nf.getNumOps()) {
-            left = nf.getOperator(rand);
-            left.depth = depth + 1;
-            left.addRandomKids(nf, maxDepth, rand);
+    public double eval(double[] values) 
+    {
+        if (constant != null) {
+            return constant.eval(values);
+        } else if (variable != null) {
+            return variable.eval(values);
         } else {
-            left = nf.getTerminal(rand);
-            left.depth = depth + 1;
+            double leftVal = left.eval(values);
+            double rightVal = right.eval(values);
+            return op.eval(leftVal, rightVal);
         }
-
-        int choiceRight = rand.nextInt(nf.getNumOps() + nf.getNumIndepVars() + 1);
-        if (choiceRight < nf.getNumOps()) {
-            right = nf.getOperator(rand);
-            right.depth = depth + 1;
-            right.addRandomKids(nf, maxDepth, rand);
-        } else {
-            right = nf.getTerminal(rand);
-            right.depth = depth + 1;
-        }
-    }
-    
-    public double eval(double[] values) {
-        if (operation instanceof Binop) {
-            Binop b = (Binop) operation;
-            return b.eval(left.eval(values), right.eval(values));
-        } else if (operation instanceof Unop) {
-            Unop u = (Unop) operation;
-            return u.eval(values);
-        }
-        return 0;
-    }
-    public Object clone() {
-        Object o = null;
-        try {
-            o = super.clone();
-        } catch (CloneNotSupportedException e) {
-            System.out.println("Op can't clone.");
-        }
-        Node b = (Node) o;
-        if (left != null) b.left = (Node) left.clone();
-        if (right != null) b.right = (Node) right.clone();
-        if (operation != null) b.operation = (Op) operation.clone();
-        return b;
     }
 
-    public String toString() {
-        if (operation instanceof Binop) {
-            return "(" + left.toString() + " " + operation.toString() + " " + right.toString() + ")";
-        } else {
-            return operation.toString();
-        }
+    public String toString() 
+    {
+        if (constant != null) return constant.toString();
+        if (variable != null) return variable.toString();
+        return "(" + left.toString() + " " + op.toString() + " " + right.toString() + ")";
+    }
+
+   
+    public void traverse(Collector c) 
+    {
+        c.collect(this);  // collect this node first (preorder)
+        if (left != null) left.traverse(c);
+        if (right != null) right.traverse(c);
+    }
+   
+    public void swapLeft(Node trunk) 
+    {
+        Node temp = this.left;
+        this.left = trunk.left;
+        trunk.left = temp;
+    }
+
+    public void swapRight(Node trunk) 
+    {
+        Node temp = this.right;
+        this.right = trunk.right;
+        trunk.right = temp;
+    }
+
+    public boolean isLeaf() 
+    {
+        return (left == null && right == null);
     }
 }
 

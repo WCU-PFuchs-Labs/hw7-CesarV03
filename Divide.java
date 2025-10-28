fbe64994cd8cd9020e8a26e8243765cce4c2075c
CesarV03 <3021762@wcupa.edu>
2025-10-28 17:27:27 -0400

    Divide.java

--- Divide.java
+++ Divide.java
@@ -1,7 +1,9 @@
 public class Divide extends Binop {
     public double eval(double left, double right) 
     {
-        if (right == 0) return 0;  
+        if (Math.abs(right) < 0.0001) {
+            return 1.0;
+        }
         return left / right;
     }
 

824744dcb1a25acbb36148e0f782fb608d23ca1f
CesarV03 <3021762@wcupa.edu>
2025-10-28 17:00:42 -0400

    Op.java

---
+++ Op.java
@@ -0,0 +1,11 @@
+public class Op implements Cloneable {
+    public Object clone() {
+        Object o = null;
+        try {
+            o = super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Op can't clone.");
+        }
+        return o;
+    }
+}

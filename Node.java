cc657170665540bcd5904fa2fcbbbdf52b6dbff2
CesarV03 <3021762@wcupa.edu>
2025-10-28 20:14:33 -0400

    Node.java

--- Node.java
+++ Node.java
@@ -1,19 +1,19 @@
 // imported first from grasp
-import java.util.*;
+import java.util.Random;
 
 public class Node {
 
     protected Node lChild;
     protected Node rChild;
-    protected Binop op;
-    protected Const c;
-    protected Variable v;
+    protected Binop op;        
+    protected Const c;         
+    protected Variable v;     
 
     public Node(Const c) 
     {
         this.c = c;
-        this.op = null;
         this.v = null;
+        this.op = null;
         this.lChild = null;
         this.rChild = null;
     }
@@ -21,54 +21,53 @@ public class Node {
     public Node(Variable v) 
     {
         this.v = v;
-        this.op = null;
         this.c = null;
+        this.op = null;
         this.lChild = null;
         this.rChild = null;
+    }
+
+    public Node(Binop op, Node left, Node right) 
+    {
+        this.op = op;
+        this.lChild = left;
+        this.rChild = right;
+        this.c = null;
+        this.v = null;
     }
 
     public Node(Binop op) 
     {
         this.op = op;
-        this.c = null;
-        this.v = null;
         this.lChild = null;
         this.rChild = null;
+        this.c = null;
+        this.v = null;
     }
+
 
     public double eval(double[] data) 
     {
-        if (c != null) {
-            return c.eval(data);
-        } else if (v != null) {
-            return v.eval(data);
-        } else if (op != null) {
-            return op.eval(lChild, rChild, data);
-        }
-        return 0.0;
+        if (c != null) return c.eval(data);
+        if (v != null) return v.eval(data);
+        double leftVal = lChild.eval(data);
+        double rightVal = rChild.eval(data);
+        return op.eval(leftVal, rightVal);
     }
 
     public String toString() 
     {
-        if (c != null) {
-            return c.toString();
-        } else if (v != null) {
-            return v.toString();
-        } else if (op != null) {
-            return "(" + lChild + " " + op + " " + rChild + ")";
-        }
-        return "";
+        if (c != null) return c.toString();
+        if (v != null) return v.toString();
+        return "(" + lChild.toString() + " " + op.toString() + " " + rChild.toString() + ")";
     }
 
-    public void traverse(Collector c) 
+
+    public void traverse(Collector collector) 
     {
-        c.collect(this);
-        if (lChild != null) {
-            lChild.traverse(c);
-        }
-        if (rChild != null) {
-            rChild.traverse(c);
-        }
+        collector.collect(this);
+        if (lChild != null) lChild.traverse(collector);
+        if (rChild != null) rChild.traverse(collector);
     }
 
     public void swapLeft(Node trunk) 
@@ -77,7 +76,7 @@ public class Node {
         this.lChild = trunk.lChild;
         trunk.lChild = temp;
     }
-
+    
     public void swapRight(Node trunk) 
     {
         Node temp = this.rChild;
@@ -90,20 +89,40 @@ public class Node {
         return (op == null);
     }
 
-    public void addRandomKids(NodeFactory n, int maxDepth, Random rand) 
+    public void addRandomKids(NodeFactory nf, int maxDepth, Random rand) 
     {
         if (isLeaf() || maxDepth <= 0) return;
+        int numOps = nf.getNumOps();
+        int numVars = nf.getNumIndepVars();
+        int choices = numOps + numVars + 1;
+
         if (lChild == null) {
-            Node child = n.getRandomNode(rand, maxDepth - 1);
-            lChild = child;
-            lChild.addRandomKids(n, maxDepth - 1, rand);
+            if (maxDepth == 1) 
+            {
+                lChild = nf.getTerminal(rand);
+            } else {
+                int choice = rand.nextInt(choices);
+                if (choice < numOps) {
+                    lChild = nf.getOperator(rand);
+                    lChild.addRandomKids(nf, maxDepth - 1, rand);
+                } else {
+                    lChild = nf.getTerminal(rand);
+                }
+            }
         }
 
         if (rChild == null) {
-            Node child = n.getRandomNode(rand, maxDepth - 1);
-            rChild = child;
-            rChild.addRandomKids(n, maxDepth - 1, rand);
+            if (maxDepth == 1) {
+                rChild = nf.getTerminal(rand);
+            } else {
+                int choice = rand.nextInt(choices);
+                if (choice < numOps) {
+                    rChild = nf.getOperator(rand);
+                    rChild.addRandomKids(nf, maxDepth - 1, rand);
+                } else {
+                    rChild = nf.getTerminal(rand);
+                }
+            }
         }
     }
 }
-   
